ef97088a341ff252feb97f3883bb02d411eeb5b8
createqiqi <57378554+createqiqi@users.noreply.github.com>
2019-11-05 10:26:22 +0800

    ES6的学习
    上传了一个ES6的学习

---
+++ js/ES61/index.js
@@ -0,0 +1,36 @@
+//使用链接符号 
+/* let name = "河北传媒";
+console.log('你好' +':'+ name+':' +'毕业了');
+console.log(`你好,
+
+
+${name}
+
+欢迎你111`);
+//链接符号支持运算符
+let a =1;
+let b =2;
+let result = a+b;
+document.write(`${result}   你好 `); */
+//解构赋值
+/* let arr1=['ac',2,'d'];
+let arr2= [...arr1];
+arr2.push(4);
+console.log(arr1[0]);
+console.log(arr2); */
+/* let [a=1,b] = [];
+console.log(b); */
+//字符串
+/* let string = "I am start banana,orange,sing,haha";
+let rusult = string.includes("banana");//是否能找到含banana的参数
+let rusult2 = string.endsWith("orange");//字符串是否在尾部
+let rusult3 = string.startsWith("I am start");//字符串是否在头部
+let rusult4 = string.startsWith("start",5);//空格也算一个值
+console.log(rusult);
+console.log(rusult2);
+console.log(rusult3);
+console.log(rusult4); */
+//字符串重复
+//console.log("feidu love luowenzhou ,too".repeat(3));
+let arr =["feidu","luowenzhou","luowenzhou is sunshine for feidu"]
+document.write(arr.join(" and "));
